6d5f61082208ded39a481c0f11d532675e50ff4e
Vladislavs Golubs <vladislavs.golubs@yandex.ru>
2020-01-22 17:33:51 +0300

    Add optional schema

--- dialog-storage/src/main/scala/im/dlg/storage/SimpleStorage.scala
+++ dialog-storage/src/main/scala/im/dlg/storage/SimpleStorage.scala
@@ -6,26 +6,26 @@ import scala.concurrent.duration.FiniteDuration
 trait Connector {
   def run[R](action: api.Action[R]): Future[R]
 
-  def createTableIfNotExists(name: String, createReverseIndex: Boolean): Unit
+  def createTableIfNotExists(schema: Option[String] = None, name: String, createReverseIndex: Boolean): Unit
 
   final def runSync[R](action: api.Action[R])(implicit timeout: FiniteDuration): R =
     Await.result(run(action), timeout)
 }
 
-class SimpleStorage(val name: String) {
+class SimpleStorage(schema: Option[String] = None, val name: String) {
   import api._
 
-  final def get(key: String) = GetAction(name, key)
+  final def get(key: String) = GetAction(schema, name, key)
 
-  final def getByPrefix(keyPrefix: String) = GetByPrefixAction(name, keyPrefix)
+  final def getByPrefix(keyPrefix: String) = GetByPrefixAction(schema, name, keyPrefix)
 
-  final def upsert(key: String, value: Array[Byte]) = UpsertAction(name, key, value)
+  final def upsert(key: String, value: Array[Byte]) = UpsertAction(schema, name, key, value)
 
-  final def delete(key: String) = DeleteAction(name, key)
+  final def delete(key: String) = DeleteAction(schema, name, key)
 
-  final def getKeys = GetKeysAction(name)
+  final def getKeys = GetKeysAction(schema, name)
 
-  final def getKeysForValue(value: Array[Byte]) = GetKeysForValue(name, value)
+  final def getKeysForValue(value: Array[Byte]) = GetKeysForValue(schema, name, value)
 }
 
 object api {
@@ -33,16 +33,16 @@ object api {
     def name: String
   }
 
-  final case class GetAction(name: String, key: String) extends Action[Option[Array[Byte]]]
+  final case class GetAction(schema: Option[String], name: String, key: String) extends Action[Option[Array[Byte]]]
 
-  final case class GetByPrefixAction(name: String, keyPrefix: String) extends Action[Vector[(String, Array[Byte])]]
+  final case class GetByPrefixAction(schema: Option[String], name: String, keyPrefix: String) extends Action[Vector[(String, Array[Byte])]]
 
-  final case class UpsertAction(name: String, key: String, value: Array[Byte]) extends Action[Int]
+  final case class UpsertAction(schema: Option[String], name: String, key: String, value: Array[Byte]) extends Action[Int]
 
-  final case class DeleteAction(name: String, key: String) extends Action[Int]
+  final case class DeleteAction(schema: Option[String], name: String, key: String) extends Action[Int]
 
-  final case class GetKeysAction(name: String) extends Action[Seq[String]]
+  final case class GetKeysAction(schema: Option[String], name: String) extends Action[Seq[String]]
 
-  final case class GetKeysForValue(name: String, value: Array[Byte]) extends Action[Vector[String]]
+  final case class GetKeysForValue(schema: Option[String], name: String, value: Array[Byte]) extends Action[Vector[String]]
 
 }
